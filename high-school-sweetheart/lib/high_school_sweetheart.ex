ba1e2c063d39c2f5b070dfb371fecbe1ebe12291
Ayiko Fred <lgmfred@ayikoyo.com>
2022-08-30 20:17:35 +0300

    High school sweetheart

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,45 @@
+defmodule HighSchoolSweetheart do
+  @spec first_letter(String.t()) :: String.t()
+  def first_letter(name) do
+    name
+    |> String.trim()
+    |> String.first()
+  end
+
+  @spec initial(name :: String.t()) :: String.t()
+  def initial(name) do
+    first_letter(name)
+    |> String.upcase()
+    |> Kernel.<>(".")
+  end
+
+  @spec initials(String.t()) :: String.t()
+  def initials(full_name) do
+    String.split(full_name)
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
+  end
+
+  @spec pair(String.t(), String.t()) :: String.t()
+  def pair(full_name1, full_name2) do
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{i1}  +  #{i2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
